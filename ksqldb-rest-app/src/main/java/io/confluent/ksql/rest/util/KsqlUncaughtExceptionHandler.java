555143838efe7ab660a2b73a1c9edfad76933828
Steven Zhang <stevenz@confluent.io>
2021-04-20 14:02:18 -0700

    update test

--- ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/util/KsqlUncaughtExceptionHandler.java
+++ ksqldb-rest-app/src/main/java/io/confluent/ksql/rest/util/KsqlUncaughtExceptionHandler.java
@@ -15,23 +15,40 @@
 
 package io.confluent.ksql.rest.util;
 
+import com.google.common.annotations.VisibleForTesting;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.confluent.ksql.rest.server.KsqlServerMain;
+import org.apache.kafka.streams.processor.internals.StreamThread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class KsqlUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
   private final Runnable flusher;
+  private final String streamsThreadMessage;
 
   private static final Logger log = LoggerFactory.getLogger(KsqlServerMain.class);
 
   public KsqlUncaughtExceptionHandler(final Runnable flusher) {
+    this(flusher, "");
+  }
+
+  public KsqlUncaughtExceptionHandler(
+      final Runnable flusher,
+      final String streamsThreadMessage
+  ) {
     this.flusher = flusher;
+    this.streamsThreadMessage = streamsThreadMessage;
   }
 
   @SuppressFBWarnings
   public void uncaughtException(final Thread t, final Throwable e) {
+    if (t instanceof StreamThread) {
+      if (!streamsThreadMessage.isEmpty()) {
+        System.err.println(streamsThreadMessage);
+      }
+      return;
+    }
     log.error("Unhandled exception caught in thread {}.", t.getName(), e);
     System.err.println(
         "Unhandled exception caught in thread: " + t.getName() + ". Exception:" + e.getMessage());
